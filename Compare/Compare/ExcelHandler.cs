a77cc6b6e2c10d880e0b960f77ffc238fa04eb51
DESKTOP-5P76Q28\Thomas <kontakt@rosendahlphotos.dk>
2017-05-25 22:35:14 +0200

    V. 1.01

---
+++ Compare/Compare/ExcelHandler.cs
@@ -0,0 +1,69 @@
+﻿using OfficeOpenXml;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Compare
+{
+    class ExcelHandler
+    {
+        private string ExcelFilePath { get; set; }
+        private List<string> FileNames { get; set; }
+
+        public ExcelHandler(string filePath, string lookFolderFilePath)
+        {
+            ExcelFilePath = filePath;
+            FileNames = GetFilesNames(lookFolderFilePath);
+        }
+
+        private List<string> GetFilesNames(string lookFolderFilePath)
+        {
+            var result = new List<string>();
+            var fileNames = Directory.GetFiles(lookFolderFilePath).ToList();
+            foreach(var fileName in fileNames)
+            {
+                var name = Path.GetFileNameWithoutExtension(fileName);
+                result.Add(name);
+            }
+
+            return result;
+        }
+
+        public List<string> StartExcelWork()
+        {
+            var result = new List<string>();
+
+            try
+            {
+                using (var file = File.Open(ExcelFilePath, FileMode.Open))
+                {
+                    using (var package = new ExcelPackage(file))
+                    {
+                        var workbook = package.Workbook;
+                        var worksheet = workbook.Worksheets[ConfigurationManager.AppSettings["WorkSheet"]];
+                        var end = worksheet.Dimension.End;
+
+                        for (int i = 1; i < end.Row; i++)
+                        {
+                            var person = worksheet.Cells[i + 1, 3].Value + " " + worksheet.Cells[i + 1, 4].Value;
+
+                            if (!FileNames.Exists(x => x.Replace(" ", "").ToLower().Contains(person.Replace(" ", "").ToLower())))
+                            {
+                                result.Add(person);
+                            }
+                        }
+                    }
+                }
+                return result;
+            }
+            catch (Exception e)
+            {
+                return new List<string> { "File is open!" };
+            }
+        }
+    }
+}
---
+++ Compare/Compare/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System.Windows.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Compare
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public List<string> ExcelItems = new List<string>();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ReadExcelClick(object sender, RoutedEventArgs e)
+        {
+            ExcelListBox.Items.Clear();
+
+            if (string.IsNullOrEmpty(LookupFolderLabel.Content as string))
+                return;
+
+            var openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Excel Files (*.xlsx)|*.xlsx";
+            var dialogResult = openFileDialog.ShowDialog();
+            
+            if (dialogResult == System.Windows.Forms.DialogResult.OK)
+            {
+                var filePath = openFileDialog.FileName;
+                var lookupFolder = LookupFolderLabel.Content as string;
+                var excelHandler = new ExcelHandler(filePath, lookupFolder);
+
+                var items = excelHandler.StartExcelWork();
+                foreach(var item in items)
+                {
+                    ListBoxItem itm = new ListBoxItem();
+                    itm.Content = item;
+
+                    ExcelListBox.Items.Add(itm);
+                }
+            }
+        }
+
+        private void LookupFolderClick(object sender, RoutedEventArgs e)
+        {
+            var fbd = new FolderBrowserDialog();
+            if (fbd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                LookupFolderLabel.Content = fbd.SelectedPath;
+            }
+        }
+    }
+}
